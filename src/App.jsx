175ab9914f0d624ac362efbba641909de02da9c2
munyite001 <emunyite@gmail.com>
2024-01-06 20:03:45 +0300

    React router tutorial

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom"
+const App = () => {
+  return (
+    <div>
+      <h1>Hello from the main page of the app!</h1>
+      <p>Here are some examples of links to other pages</p>
+      <nav>
+        <ul>
+          <li>
+            <Link to="profile">Profile Page</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default App;
